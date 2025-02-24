0dac122f5a138fbe9c8b0e2581cd30ffffba5c12
Chinedu Amaechi <chymez56@yahoo.com>
2025-02-23 17:35:21 -0700

    Updating the SignUp Form

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import Chatbot from "./features/Chatbot";
 import { Toaster } from "react-hot-toast";
 import { CartProvider } from "./ui/CartContext";
 import NavBar from "./ui/Navbar";
-import Footer from "./ui/Fcooter";
+import Footer from "./ui/Footer";
 import "./globals.css";
 
 const openSans = Open_Sans({
